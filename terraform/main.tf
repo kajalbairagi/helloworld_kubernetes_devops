43105edb2f803ec16841a40d840f5f8f88ee959f
kajalbairagi <100835131+kajalbairagi@users.noreply.github.com>
2025-10-16 21:27:42 +0530

    nothing
    eks_managed_node_groups = {
        default = {
          desired_capacity = var.node_group_desired_capacity
          max_capacity     = var.node_group_desired_capacity + 1
          min_capacity     = 1
          instance_types   = ["t3.medium"]
        }

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,13 +24,7 @@ module "eks" {
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
-   eks_managed_node_groups = {
-    default = {
-      desired_capacity = var.node_group_desired_capacity
-      max_capacity     = var.node_group_desired_capacity + 1
-      min_capacity     = 1
-      instance_types   = ["t3.medium"]
-    }
+   
   }
 
  enable_cluster_creator_admin_permissions = true
